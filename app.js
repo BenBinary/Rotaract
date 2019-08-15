63532382a44382836d006fe73dda4fb0923ee61f
BenBinary94 <bk191414@gmail.com>
2019-08-15 16:39:18 +0200

    Einrichten einer Struktur für express und ejs

---
+++ app.js
@@ -0,0 +1,42 @@
+
+// Variablendeklaration
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const test = fs.readFileSync("./test.html", {encoding: "utf-8"});
+const login = fs.readFileSync("./login.html", {encoding: "utf-8"});
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(function(req, res, next) {
+
+
+    const url = req.url;
+
+
+    if (url === "/") {
+
+        console.log("In / Abzwerigung");
+        //res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        res.send(login);
+
+    } else if (url === "/events") {
+
+        const events = fs.readFileSync("./views/basic.ejs", {encoding: "utf-8"});
+        
+        res.render(events, {});
+
+    }
+
+    
+
+});
+
+app.listen(5400, function() {
+    console.log("Konsoloe lauscht auf Port 5400");
+});
---
+++ sonstiges.js
@@ -0,0 +1,23 @@
+/*
+const server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+
+    if (url === "/") {
+
+        response.write(html);
+        response.end();
+    }
+
+    const url = request.url;
+    console.log(url);
+});
+
+
+console.log(login);
+server.listen(5400, function() {
+
+    console.log("Ich bin jetzt auf Port 5400 geschaltet");
+});
+
+*/
